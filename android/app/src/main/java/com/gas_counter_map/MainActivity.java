c5140b235858cbf2c81bced6d0e0a443b717425d
Alexandr Kovtunov <1sasha.sasha1@gmail.com>
2016-06-02 00:13:02 +0300

    Working react-native-maps example

--- android/app/src/main/java/com/gas_counter_map/MainActivity.java
+++ android/app/src/main/java/com/gas_counter_map/MainActivity.java
@@ -1,8 +1,10 @@
 package com.gas_counter_map;
 
 import com.facebook.react.ReactActivity;
+import com.AirMaps.AirPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.airbnb.android.react.maps.MapsPackage;//  <-- add this
 
 import java.util.Arrays;
 import java.util.List;
@@ -34,7 +36,9 @@ public class MainActivity extends ReactActivity {
     @Override
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
-            new MainReactPackage()
+            new MainReactPackage(),
+            new AirPackage(),
+            new MapsPackage()
         );
     }
 }
--- index.android.js
+++ index.android.js
@@ -4,6 +4,9 @@
  */
 
 import React, { Component } from 'react';
+
+import MapView from 'react-native-maps';
+
 import {
   AppRegistry,
   StyleSheet,
@@ -15,21 +18,19 @@ class gas_counter_map extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
+      <MapView
+        style={styles.map}
+      />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  map: {
+    height: 150,
+    alignSelf: 'stretch',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
